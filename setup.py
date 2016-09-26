203f98300d9f8194dfbbaef8b3ad33159421b685
Hugo Rodger-Brown <hugo@rodger-brown.com>
2016-09-26 16:27:05 -0400

    Bump version to 0.3.1
    Added missing migration.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ chdir(path.normpath(path.join(path.abspath(__file__), pardir)))
 
 setup(
     name="elasticsearch-django",
-    version="0.3.0",
+    version="0.3.1",
     packages=find_packages(),
     install_requires=[
         'django>=1.8',
