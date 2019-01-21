d9e00fffcb59214c1ddb6d6e282ffd3c1d3ccc29
Hugo Rodger-Brown <hugo@yunojuno.com>
2019-01-21 16:09:56 +0000

    Update disable_search_updates decorator

--- elasticsearch_django/utils.py
+++ elasticsearch_django/utils.py
@@ -1,23 +1,7 @@
-from contextlib import contextmanager
+import warnings
 
-from .settings import get_settings
-
-
-@contextmanager
-def disable_search_updates():
-    """
-    Context manager used to temporarily disable auto_sync.
-
-    This is useful when performing bulk updates on objects - when
-    you may not want to flood the indexing process.
-
-    >>> with disable_search_updates():
-    ...     for obj in model.objects.all():
-    ...     obj.save()
-
-    """
-    _settings = get_settings()
-    _sync = _settings["auto_sync"]
-    _settings["auto_sync"] = False
-    yield
-    _settings["auto_sync"] = _sync
+warnings.warn(
+    "Utils module is deprecated, please import disable_search_updates "
+    "please import disable_search_updates from decorators instead.",
+    DeprecationWarning,
+)
