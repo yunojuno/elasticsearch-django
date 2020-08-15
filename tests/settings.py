d0f223974b4b22e4f2cc720734fd6ce7665a9720
Hugo Rodger-Brown <hugo@yunojuno.com>
2020-08-15 14:45:33 +0100

    Add sqlite or postgres settings based on JSONField import

--- tests/settings.py
+++ tests/settings.py
@@ -2,17 +2,23 @@ from os import getenv
 
 DEBUG = True
 
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "test.db",}}
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": getenv("TEST_DB_NAME", "elasticsearch_django"),
-#         "USER": getenv("TEST_DB_USER", "postgres"),
-#         "PASSWORD": getenv("TEST_DB_PASSWORD", "postgres"),
-#         "HOST": getenv("TEST_DB_HOST", "localhost"),
-#         "PORT": getenv("TEST_DB_PORT", "5432"),
-#     }
-# }
+try:
+    import django.db.models.JSONField
+
+    DATABASES = {
+        "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "test.db",}
+    }
+except ImportError:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": getenv("TEST_DB_NAME", "elasticsearch_django"),
+            "USER": getenv("TEST_DB_USER", "postgres"),
+            "PASSWORD": getenv("TEST_DB_PASSWORD", "postgres"),
+            "HOST": getenv("TEST_DB_HOST", "localhost"),
+            "PORT": getenv("TEST_DB_PORT", "5432"),
+        }
+    }
 
 INSTALLED_APPS = (
     "django.contrib.admin",
