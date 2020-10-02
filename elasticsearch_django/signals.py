0a4ccb8f64e1b4311b2d158d1e27af449f3bbd22
Hugo Rodger-Brown <hugorodgerbrown@users.noreply.github.com>
2020-10-02 13:00:19 +0000

    Remove providing_args from Signals
    The providing_args kwargs is informational and has no
    functional value. It is scheduled for deprecation in
    Django 4.0 and raises a warning in the checks. I have
    removed the kwarg and put it into a docstring instead.

--- elasticsearch_django/signals.py
+++ elasticsearch_django/signals.py
@@ -1,12 +1,13 @@
 import django.dispatch
 
 # signal fired just before calling model.index_search_document
-pre_index = django.dispatch.Signal(providing_args=["instance", "index"])
+# providing_args=["instance", "index"]
+pre_index = django.dispatch.Signal()
 
 # signal fired just before calling model.update_search_document
-pre_update = django.dispatch.Signal(
-    providing_args=["instance", "index", "update_fields"]
-)
+# providing_args=["instance", "index", "update_fields"]
+pre_update = django.dispatch.Signal()
 
 # signal fired just before calling model.delete_search_document
-pre_delete = django.dispatch.Signal(providing_args=["instance", "index"])
+# providing_args=["instance", "index"]
+pre_delete = django.dispatch.Signal()
