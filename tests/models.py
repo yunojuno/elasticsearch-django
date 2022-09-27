aa8f52c6e92c3a0c7108dce40f02183be67a72ef
tahakhan <tahakhan2189@gmail.com>
2022-09-27 11:25:34 -0400

    Add an example model with custom primary key
    Prior to this change, the examples didn't include a model with a custom primary key.
    
    This change adds it. The main purpose is to test if the in_search_queryset function works if the given instance doesn't have an id column.

--- tests/models.py
+++ tests/models.py
@@ -31,3 +31,27 @@ class ExampleModel(SearchDocumentMixin, models.Model):
             "complex_field": str(self.complex_field),
             "user_name": self.user_name(),
         }
+
+
+class ExampleModelWithCustomPrimaryKey(SearchDocumentMixin, models.Model):
+    """Model class with a custom primary key for use in tests."""
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True
+    )
+    simple_field_1 = models.IntegerField(primary_key=True)
+    simple_field_2 = models.CharField(max_length=100)
+    complex_field = models.FileField()
+
+    objects = ExampleModelManager()
+
+    def user_name(self) -> str:
+        return self.user.get_full_name() if self.user else "Anonymous"
+
+    def as_search_document(self, index="_all"):
+        return {
+            "simple_field_1": self.simple_field_1,
+            "simple_field_2": self.simple_field_2,
+            "complex_field": str(self.complex_field),
+            "user_name": self.user_name(),
+        }
