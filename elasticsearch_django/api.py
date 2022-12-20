6436f2c12fa3fc8ec23ea15b45523f16bf19def4
Hugo Rodger-Brown <hugo@yunojuno.com>
2022-12-20 10:30:43 +0000

    WIP

--- elasticsearch_django/api.py
+++ elasticsearch_django/api.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import datetime
-from typing import TypeAlias, cast
+from typing import Any, TypeAlias, cast
 
 from django.conf import settings
 from elasticsearch import Elasticsearch
@@ -20,14 +20,8 @@ DEFAULT_PAGE_SIZE = cast(int, get_setting("page_size"))
 SearchHitMetaType: TypeAlias = dict[str, str | float]
 
 
-class Search:
-    """
-    Parse search response to make it easier to work with.
-
-    This class provides a classmethod to execute a search and parse the
-    results.
-
-    """
+class BaseOperation:
+    """Base class used to execute and log search queries."""
 
     def __init__(
         self,
@@ -40,26 +34,118 @@ class Search:
         self.index = index
         self.query = query
         self.response = response
-        self._hits = response.get("hits", {})
-        self._total = self._hits.get("total", {})
         self.duration = duration
         self.executed_at = executed_at
 
-    def _extract_hit(self, hit: dict) -> dict:
-        return {
-            "index": hit["_index"],
-            "id": hit["_id"],
-            "score": hit["_score"],
-        }
-
     @property
     def hits(self) -> list[SearchHitMetaType]:
-        """Return list of id, index, score dict for each hit returned."""
-        return [self._extract_hit(h) for h in self._hits.get("hits", {})]
+        raise NotImplementedError
 
     @property
     def aggregations(self) -> dict:
-        """Return raw aggregations from the response."""
+        raise NotImplementedError
+
+    @property
+    def max_score(self) -> float:
+        raise NotImplementedError
+
+    @property
+    def total_hits(self) -> int:
+        raise NotImplementedError
+
+    @property
+    def total_hits_relation(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def query_type(self) -> str:
+        raise NotImplementedError
+
+    @classmethod
+    def do_execute(
+        cls,
+        *,
+        index: str | list[str],
+        query: dict,
+        client: Elasticsearch = DEFAULT_CLIENT,
+        **kwargs: Any,
+    ) -> dict:
+        raise NotImplementedError
+
+    @classmethod
+    def execute(
+        cls,
+        *,
+        index: str | list[str],
+        query: dict,
+        client: Elasticsearch = DEFAULT_CLIENT,
+        **kwargs: Any,
+    ) -> BaseOperation:
+        with stopwatch() as timer:
+            response = cls.do_execute(index=index, query=query, client=client, **kwargs)
+        return cls(
+            index=index,
+            query=query,
+            response=response,
+            duration=timer.elapsed,
+            executed_at=timer.started_at,
+        )
+
+    def save(
+        self,
+        user: settings.AUTH_USER_MODEL | None = None,
+        search_terms: str = "",
+        reference: str = "",
+    ) -> SearchQuery:
+        return SearchQuery.objects.create(
+            user=user,
+            search_terms=search_terms,
+            reference=reference,
+            index=self.index,
+            query=self.query,
+            query_type=self.query_type,
+            hits=self.hits,
+            aggregations=self.aggregations,
+            total_hits=self.total_hits,
+            total_hits_relation=self.total_hits_relation,
+            executed_at=self.executed_at,
+            duration=self.duration,
+        )
+
+
+class Search(BaseOperation):
+    """
+    Parse search response to make it easier to work with.
+
+        >>> api.Search.execute(index=index, query=query).save()
+
+    """
+
+    def __init__(
+        self,
+        index: str | list[str],
+        query: dict,
+        response: dict,
+        duration: float = 0.0,
+        executed_at: datetime.datetime | None = None,
+    ) -> None:
+        super().__init__(index, query, response, duration, executed_at)
+        self._hits = response.get("hits", {})
+        self._total = self._hits.get("total", {})
+
+    @property
+    def hits(self) -> list[SearchHitMetaType]:
+        return [
+            {
+                "index": h["_index"],
+                "id": h["_id"],
+                "score": h["_score"],
+            }
+            for h in self._hits.get("hits", {})
+        ]
+
+    @property
+    def aggregations(self) -> dict:
         return self.response.get("aggregations", {})
 
     @property
@@ -74,42 +160,59 @@ class Search:
     def total_hits_relation(self) -> str:
         return self._total.get("relation", "")
 
+    @property
+    def query_type(self) -> str:
+        return str(SearchQuery.QueryType.SEARCH)
+
     @classmethod
-    def execute(
+    def do_execute(
         cls,
         *,
         index: str | list[str],
         query: dict,
         client: Elasticsearch = DEFAULT_CLIENT,
-    ) -> Search:
-        with stopwatch() as timer:
-            response = client.search(index=index, **query)
-        search = Search(
-            index=index,
-            query=query,
-            response=response,
-            duration=timer.elapsed,
-            executed_at=timer.started_at,
-        )
-        return search
+        **kwargs: Any,
+    ) -> dict:
+        kwargs.setdefault("from_", DEFAULT_FROM)
+        kwargs.setdefault("size", DEFAULT_PAGE_SIZE)
+        return client.search(index=index, query=query, **kwargs)
 
-    def log(
-        self,
-        user: settings.AUTH_USER_MODEL,
-        search_terms: str = "",
-        reference: str = "",
-    ) -> SearchQuery:
-        return SearchQuery.objects.create(
-            user=user,
-            search_terms=search_terms,
-            index=self.index,
-            query=self.query,
-            query_type=SearchQuery.QueryType.SEARCH,
-            hits=self.hits,
-            aggregations=self.aggregations,
-            total_hits=self.total_hits,
-            total_hits_relation=self.total_hits_relation,
-            reference=reference or "",
-            executed_at=self.executed_at,
-            duration=self.duration,
-        )
+
+class Count(BaseOperation):
+    """
+    Parse count response to make it easier to work with.
+
+        >>> api.Count.execute(index=index, query=query).save()
+
+    """
+
+    @property
+    def hits(self) -> list[SearchHitMetaType]:
+        return []
+
+    @property
+    def aggregations(self) -> dict:
+        return {}
+
+    @property
+    def total_hits(self) -> int:
+        return self.response.get("count", 0)
+
+    @property
+    def total_hits_relation(self) -> str:
+        return str(SearchQuery.TotalHitsRelation.ACCURATE)
+
+    @property
+    def query_type(self) -> str:
+        return str(SearchQuery.QueryType.COUNT)
+
+    @classmethod
+    def do_execute(
+        cls,
+        *,
+        index: str | list[str],
+        query: dict,
+        client: Elasticsearch = DEFAULT_CLIENT,
+        **kwargs: Any,
+    ) -> dict:
+        return client.count(index=index, query=query, **kwargs)
